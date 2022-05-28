dc1b1e71c748a217694aa8542993da6b11b522d0
I am Sheep <jorgexiaoyang@gmail.com>
2022-05-27 17:41:41 -0700

    nav css

--- frontend/src/componenets/Navigation/index.js
+++ frontend/src/componenets/Navigation/index.js
@@ -3,32 +3,40 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
-// import "./Navigation.css";
+import "./Navigation.css";
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector((state) => state.session.user);
 
   let sessionLinks;
-  if (sessionUser) {
-    sessionLinks = <ProfileButton user={sessionUser} />;
-  } else {
-    sessionLinks = (
-      <>
-        <NavLink to="/login">Log In</NavLink>
-        <NavLink to="/signup">Sign Up</NavLink>
-      </>
-    );
-  }
+  // if (sessionUser) {
+  //   sessionLinks = <ProfileButton user={sessionUser} />;
+  // } else {
+  //   sessionLinks = (
+  //     <>
+  //       <NavLink to="/login">Log In</NavLink>
+
+  //       <NavLink to="/signup">Sign Up</NavLink>
+  //     </>
+  //   );
+  // }
 
   return (
-    <ul>
-      <li>
-        <NavLink exact to="/">
-          Home
-        </NavLink>
-        {isLoaded && sessionLinks}
-      </li>
-    </ul>
+    <div className="notloggedinnav">
+      <NavLink exact to="/">
+        Home
+      </NavLink>
+      {sessionUser ? (
+        <ProfileButton user={sessionUser} />
+      ) : (
+        <>
+          <NavLink to="/login">Log In</NavLink>
+
+          <NavLink to="/signup">Sign Up</NavLink>
+        </>
+      )}
+      {isLoaded && sessionLinks}
+    </div>
   );
 }
 
