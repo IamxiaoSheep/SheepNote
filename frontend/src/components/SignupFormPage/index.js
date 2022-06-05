9883b3391492a3e58b7628830cb1dad53ec90868
I am Sheep <jorgexiaoyang@gmail.com>
2022-06-05 13:28:59 -0700

    homesignuploginnav

--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import * as sessionActions from "../../store/session";
 import "./SignupForm.css";
+import sheep from "../../imgs/sheeps.gif";
 function SignupFormPage() {
   const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
@@ -33,43 +34,50 @@ function SignupFormPage() {
 
   return (
     <div className="signup">
+      <img className="animation" src={sheep} />
       <div className="signupfield">
-        <form onSubmit={handleSubmit}>
-          <ul>
-            {errors.map((error, idx) => (
-              <li key={idx}>{error}</li>
-            ))}
-          </ul>
-          <label>
+        <form className="signupform" onSubmit={handleSubmit}>
+          <div className="errorcontainer">
+            <ul className="errorssignup">
+              {errors.map((error, idx) => (
+                <li key={idx}>{error}</li>
+              ))}
+            </ul>
+          </div>
+          <label className="signuplabel">
             Email
             <input
+              className="signupinput"
               type="text"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
             />
           </label>
-          <label>
+          <label className="signuplabel">
             Username
             <input
+              className="signupinput"
               type="text"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
               required
             />
           </label>
-          <label>
+          <label className="signuplabel">
             Password
             <input
+              className="signupinput"
               type="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
             />
           </label>
-          <label>
+          <label className="signuplabel">
             Confirm Password
             <input
+              className="signupinput"
               type="password"
               value={confirmPassword}
               onChange={(e) => setConfirmPassword(e.target.value)}
