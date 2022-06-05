c4040d9b690ba7cef5839bd1dc5ed776b59ffbfc
I am Sheep <jorgexiaoyang@gmail.com>
2022-06-04 17:07:26 -0700

    css

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import * as sessionActions from "../../store/session";
+import { NavLink } from "react-router-dom";
 
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
@@ -31,15 +32,17 @@ function ProfileButton({ user }) {
 
   return (
     <>
-      <button onClick={openMenu}>
+      <li classname="ham" onClick={openMenu}>
         <i className="fas fa-user-circle" />
-      </button>
+      </li>
       {showMenu && (
-        <ul className="profile-dropdown">
+        <ul className="profilebuttoninfo">
           <li>{user.username}</li>
           <li>{user.email}</li>
           <li>
-            <button onClick={logout}>Log Out</button>
+            <button className="logout" onClick={logout}>
+              Log Out
+            </button>
           </li>
         </ul>
       )}
